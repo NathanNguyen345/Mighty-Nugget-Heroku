90b6f277d20a0e2165358758a6c81a7aa5c66b3f
Nathan Nguyen <nathannguyen345@gmail.com>
2022-01-19 15:39:36 -0800

    Fix API GET

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
 
-    app.get('*', (req, res) => {
+    app.get('/', (req, res) => {
         req.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     })
 }
