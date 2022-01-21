7e54f99ad1e04031306c7b2b7066eef4392b5cd9
Nathan Nguyen <nathannguyen345@gmail.com>
2022-01-20 20:04:53 -0800

    Test

--- server.js
+++ server.js
@@ -22,8 +22,9 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
 
-    app.get('/*', (req, res) => {
-        req.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname + '/../dist/index.html'));
+        // req.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     })
 }
 
@@ -85,5 +86,6 @@ io.on('connection', (socket) => {
         })
     })
 });
+
 
 http.listen(port, () => console.log(`Server listening on port: ${port}`));
